b1c891e5a2c20108e27499805cba240b154381b8
Tonitf <subcero00@hotmail.com>
2025-02-19 15:43:58 +0100

    cambiar home controller

--- core/App.php
+++ core/App.php
@@ -1,7 +1,7 @@
 <?php
 namespace Formacom\Core;
 class App{
-    protected $controller="Formacom\\Controllers\\HomeController";
+    protected $controller="Formacom\\Controllers\\TaskController";
     protected $method="index";
     protected $params=[];
 
