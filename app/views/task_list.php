950414796034e2df84b166033eeb288c4f293a0f
Tonitf <subcero00@hotmail.com>
2025-02-19 17:53:46 +0100

    style

--- app/views/task_list.php
+++ app/views/task_list.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/taskmvc/assets/css/style.css">
     <title>Lista de Tareas</title>
 </head>
 <body>
